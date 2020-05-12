dcad7ebce0b7c96d7958d2c3e305636347edc7d2
Eduard <yamiyugi43@gmail.com>
2020-05-12 02:51:24 -0500

    cambios al storage

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,17 +6,14 @@ use App\Comment;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Lang;
 
-class CommentController extends Controller
-{
-
-  public function show($id){
-      $data = []; 
+class CommentController extends Controller{
+    public function show($id){
+      $data = [];
       $comment = Comment::findOrFail($id);
       $data["title"] = Lang::get('messages.show');
       $data["comments"] = $comment;
-
       return view('comment.show')->with("data",$data);
-  }
+    }
 
     public function create(){
       $data = [];
@@ -25,32 +22,26 @@ class CommentController extends Controller
     }
 
     public function save(Request $request, $id){
-      
       $user_id = Auth()->user()->id;
       $validatedData = $request->validate(Comment::$createRules);
-      
       $comment=new comment; /// create model object
       $comment-> product_id = $id;
       $comment-> user_id = $user_id;
       $comment-> text = $request->text;
       $comment->save();
       return back()->with('success','Comentario agregado correctamente!');
-
    }
 
    public function comment($id){
-        $data = []; 
-        $comment = Comment::findOrFail($id);
-
-        $data["title"] = Lang::get('messages.comment');
-        $data["comments"] = $comment;
-
-        return view('comment.comment')->with("data",$data);
+      $data = [];
+      $comment = Comment::findOrFail($id);
+      $data["title"] = Lang::get('messages.comment');
+      $data["comments"] = $comment;
+      return view('comment.comment')->with("data",$data);
     }
 
     public function destroy($id) {
       $id->delete();
-
       return redirect()->route('product.index');
   }
 }
